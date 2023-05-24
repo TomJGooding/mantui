2368c34373591a037cdda537fe4867ff07e28fd1
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-05-24 22:25:52 +0100

    feat: get man page nroff content

--- src/mantui/mantui.py
+++ src/mantui/mantui.py
@@ -28,16 +28,33 @@ def get_man_page_filepath(command: str) -> str:
     return result
 
 
+def get_nroff_content(man_page_filepath: str) -> str:
+    result: str = ""
+    try:
+        out_bytes = subprocess.check_output(
+            ["zcat", man_page_filepath],
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError:
+        pass
+    else:
+        out_text = out_bytes.decode("utf-8")
+        result = out_text.strip("\n")
+
+    return result
+
+
 def main() -> None:
     args = parse_args()
     command = args.command[0]
 
-    manpath = get_man_page_filepath(command)
-    if not manpath:
+    man_page_filepath = get_man_page_filepath(command)
+    if not man_page_filepath:
         print("ERROR: No manual entry found for", command)
         return
 
-    print("Man page filepath:", manpath)
+    nroff_content = get_nroff_content(man_page_filepath)
+    print(nroff_content)
 
 
 if __name__ == "__main__":

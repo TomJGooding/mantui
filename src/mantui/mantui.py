56545bab8fc9d63198d47862a92bed6137bff94c
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-05-25 22:44:35 +0100

    feat: convert man to markdown

--- src/mantui/mantui.py
+++ src/mantui/mantui.py
@@ -4,11 +4,7 @@ import subprocess
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "command",
-        help="The command man page to view",
-        nargs=1,
-    )
+    parser.add_argument("command", help="The command man page to view")
     return parser.parse_args()
 
 
@@ -28,33 +24,62 @@ def get_man_page_filepath(command: str) -> str:
     return result
 
 
-def get_nroff_content(man_page_filepath: str) -> str:
-    result: str = ""
+def is_pandoc_installed() -> bool:
+    result: bool = False
     try:
-        out_bytes = subprocess.check_output(
-            ["zcat", man_page_filepath],
-            stderr=subprocess.STDOUT,
-        )
+        subprocess.check_output(["pandoc", "--version"])
+    except FileNotFoundError:
+        pass
     except subprocess.CalledProcessError:
         pass
     else:
-        out_text = out_bytes.decode("utf-8")
-        result = out_text.strip("\n")
+        result = True
+
+    return result
+
+
+def convert_man_to_markdown(man_page_filepath: str) -> str:
+    result: str = ""
+    try:
+        zcat = subprocess.Popen(
+            ["zcat", man_page_filepath],
+            stdout=subprocess.PIPE,
+        )
+        pandoc_out = subprocess.check_output(
+            ["pandoc", "--from", "man", "--to", "commonmark"], stdin=zcat.stdout
+        )
+    except FileNotFoundError:
+        pass
+    except subprocess.CalledProcessError:
+        pass
+    else:
+        result = pandoc_out.decode("utf-8")
 
     return result
 
 
 def main() -> None:
     args = parse_args()
-    command = args.command[0]
+    command = args.command
 
     man_page_filepath = get_man_page_filepath(command)
     if not man_page_filepath:
         print("ERROR: No manual entry found for", command)
         return
 
-    nroff_content = get_nroff_content(man_page_filepath)
-    print(nroff_content)
+    if is_pandoc_installed() is False:
+        print(
+            "ERROR: pandoc not installed.",
+            "See https://pandoc.org/installing.html",
+        )
+        return
+
+    man_markdown = convert_man_to_markdown(man_page_filepath)
+    if not man_markdown:
+        print("ERROR: Failed to convert man page for", command)
+        return
+
+    print(man_markdown)
 
 
 if __name__ == "__main__":
